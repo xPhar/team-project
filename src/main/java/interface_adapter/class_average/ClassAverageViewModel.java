5a7622c3eb008c2128639f6558d0c5425e26074e
wuqingyiWANG <wuqingyiwang@gmail.com>
2025-11-13 22:35:53 -0500

    removed redundant method and fixed viewName

--- src/main/java/interface_adapter/class_average/ClassAverageViewModel.java
+++ src/main/java/interface_adapter/class_average/ClassAverageViewModel.java
@@ -5,17 +5,8 @@ import interface_adapter.ViewModel;
 public class ClassAverageViewModel extends ViewModel<ClassAverageState> {
 
     public ClassAverageViewModel() {
-        super("class average");
+        super("average");
         this.setState(new ClassAverageState());
     }
 
-    @Override
-    public ClassAverageState getState() {
-        return super.getState();
-    }
-
-    @Override
-    public void setState(ClassAverageState state) {
-        super.setState(state);
-    }
 }
