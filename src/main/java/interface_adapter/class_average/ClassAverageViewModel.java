9e0d728c5c2794df9b27dd6d145ee86d9d37ee09
wuqingyiWANG <wuqingyiwang@gmail.com>
2025-11-13 22:26:18 -0500

    implemented view model

--- src/main/java/interface_adapter/class_average/ClassAverageViewModel.java
+++ src/main/java/interface_adapter/class_average/ClassAverageViewModel.java
@@ -1,4 +1,21 @@
 package interface_adapter.class_average;
 
-public class ClassAverageViewModel {
+import interface_adapter.ViewModel;
+
+public class ClassAverageViewModel extends ViewModel<ClassAverageState> {
+
+    public ClassAverageViewModel() {
+        super("class average");
+        this.setState(new ClassAverageState());
+    }
+
+    @Override
+    public ClassAverageState getState() {
+        return super.getState();
+    }
+
+    @Override
+    public void setState(ClassAverageState state) {
+        super.setState(state);
+    }
 }
