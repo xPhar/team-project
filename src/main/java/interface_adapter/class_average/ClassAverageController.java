f695edc00cf32b80f3743a61ae3de346c66151e8
wuqingyiWANG <wuqingyiwang@gmail.com>
2025-11-15 18:27:40 -0500

    implemented controller

--- src/main/java/interface_adapter/class_average/ClassAverageController.java
+++ src/main/java/interface_adapter/class_average/ClassAverageController.java
@@ -1,4 +1,27 @@
 package interface_adapter.class_average;
 
+import usecase.class_average.ClassAverageInputBoundary;
+import usecase.class_average.ClassAverageInputData;
+
+/**
+ * Controller for the class average use case.
+ * Called by the ClassAverageView when the user selects an assignment.
+ */
 public class ClassAverageController {
+
+    private final ClassAverageInputBoundary classAverageUseCaseInteractor;
+
+    public ClassAverageController(ClassAverageInputBoundary classAverageUseCaseInteractor) {
+        this.classAverageUseCaseInteractor = classAverageUseCaseInteractor;
+    }
+
+    /**
+     * Execute the class average use case for the selected assignment.
+     *
+     * @param assignmentName the name of the assignment chosen in the ComboBox
+     */
+    public void execute(String assignmentName) {
+        ClassAverageInputData inputData = new ClassAverageInputData(assignmentName);
+        classAverageUseCaseInteractor.execute(inputData);
+    }
 }
