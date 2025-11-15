329ee1f4d026043f51cab906e4d884bac76ebb3a
wuqingyiWANG <wuqingyiwang@gmail.com>
2025-11-15 18:29:26 -0500

    implemented inputdata

--- src/main/java/usecase/class_average/ClassAverageInputData.java
+++ src/main/java/usecase/class_average/ClassAverageInputData.java
@@ -1,4 +1,19 @@
 package usecase.class_average;
 
+/**
+ * Input Data for the Class Average Use Case.
+ * It carries the assignment name selected by the user
+ * from the Controller into the Interactor.
+ */
 public class ClassAverageInputData {
+
+    private final String assignmentName;
+
+    public ClassAverageInputData(String assignmentName) {
+        this.assignmentName = assignmentName;
+    }
+
+    public String getAssignmentName() {
+        return assignmentName;
+    }
 }
