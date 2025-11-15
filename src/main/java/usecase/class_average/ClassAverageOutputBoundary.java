59f6bad9da16b1b221eb92dc55d9cfbc9818f6f7
wuqingyiWANG <wuqingyiwang@gmail.com>
2025-11-15 18:28:50 -0500

    implemented input/output boundary

--- src/main/java/usecase/class_average/ClassAverageOutputBoundary.java
+++ src/main/java/usecase/class_average/ClassAverageOutputBoundary.java
@@ -1,4 +1,18 @@
 package usecase.class_average;
 
+/**
+ * The output boundary for the Class Average Use Case.
+ */
 public interface ClassAverageOutputBoundary {
+    /**
+     * Prepares the success view for the Class Average Use Case.
+     * @param outputData the output data
+     */
+    void prepareSuccessView(ClassAverageOutputData outputData);
+
+    /**
+     * Prepares the failure view for the Class Average Use Case.
+     * @param errorMessage the explanation of the failure
+     */
+    void prepareFailView(String errorMessage);
 }
