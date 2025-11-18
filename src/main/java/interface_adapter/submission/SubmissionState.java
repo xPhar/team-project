1e2f0fd100a8d6af38530b687cc4f0712ada98c7
Indy <48374401+Indy801@users.noreply.github.com>
2025-11-17 23:55:59 -0500

    Added submission view model and modified submission view to use it.

---
+++ src/main/java/interface_adapter/submission/SubmissionState.java
@@ -0,0 +1,47 @@
+package interface_adapter.submission;
+
+import java.time.LocalDateTime;
+
+/**
+ * The state for submission view.
+ */
+public class SubmissionState {
+    private String submitter;
+    private String status;
+    private String submissionDate;
+    private String grade;
+
+    public SubmissionState() {}
+
+    public String getSubmitter() {
+        return submitter;
+    }
+
+    public void setSubmitter(String submitter) {
+        this.submitter = submitter;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSubmissionDate() {
+        return submissionDate;
+    }
+
+    public void setSubmissionDate(String submissionDate) {
+        this.submissionDate = submissionDate;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
--- src/main/java/view/SubmissionView.java
+++ src/main/java/view/SubmissionView.java
@@ -1,16 +1,27 @@
 package view;
 
+import interface_adapter.submission.SubmissionState;
+import interface_adapter.submission.SubmissionViewModel;
+
 import javax.swing.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+/**
+ * Submission detail view.
+ */
 public class SubmissionView extends JPanel implements PropertyChangeListener {
-    private final JLabel submitterLabel = new JLabel("Submitter: Firstname Lastname");
-    private final JLabel submittedDateLabel = new JLabel("Submitted at: 11/18/2025 10:20:23");
-    private final JLabel submissionStatusLabel = new JLabel("Status: On time");
+    private final JLabel submitterLabel = new JLabel("Submitter:");
+    private final JLabel submittedDateLabel = new JLabel("Submitted at:");
+    private final JLabel submissionStatusLabel = new JLabel("Status:");
     private final JTextField gradeTextField = new JTextField();
 
-    public SubmissionView() {
+    private final SubmissionViewModel viewModel;
+
+    public SubmissionView(SubmissionViewModel viewModel) {
+        this.viewModel = viewModel;
+        this.viewModel.addPropertyChangeListener(this);
+
         final JButton downloadButton = new JButton("Download File");
         final JButton gradeButton = new JButton("Grade");
 
@@ -30,10 +41,27 @@ public class SubmissionView extends JPanel implements PropertyChangeListener {
         this.setLayout(new BoxLayout(this,  BoxLayout.Y_AXIS));
         this.add(infoPanel);
         this.add(gradePanel);
+
+        populateTestData();
+    }
+
+    private void populateTestData() {
+        viewModel.getState().setSubmitter("Indy");
+        viewModel.getState().setStatus("Grading");
+        viewModel.getState().setSubmissionDate("11/18/2025 12:30:30");
+        viewModel.getState().setGrade("20");
+        viewModel.firePropertyChange();
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
+        if (evt.getPropertyName().equals("state")) {
+            SubmissionState submissionState = (SubmissionState) evt.getNewValue();
+            submitterLabel.setText("Submitter: " + submissionState.getSubmitter());
+            submittedDateLabel.setText("Submitted at: " + submissionState.getSubmissionDate());
+            submissionStatusLabel.setText("Status: " + submissionState.getStatus());
+            gradeTextField.setText(submissionState.getGrade());
+        }
 
     }
 }
