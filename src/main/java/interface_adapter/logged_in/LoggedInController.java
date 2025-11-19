1d99d41959cb91b5aa86c062f3211d65f492d7b4
wuqingyiWANG <wuqingyiwang@gmail.com>
2025-11-19 05:51:59 -0500

    file created, tbd

--- src/main/java/interface_adapter/logged_in/LoggedInController.java
+++ src/main/java/interface_adapter/logged_in/LoggedInController.java
@@ -1,4 +1,11 @@
 package interface_adapter.logged_in;
 
 public class LoggedInController {
+
+    public LoggedInController() {}
+
+    // Fake method so LoggedInView can call something
+    public void execute() {
+        System.out.println("[FakeLoggedInController] execute() called");
+    }
 }
