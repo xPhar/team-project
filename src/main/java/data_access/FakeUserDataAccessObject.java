18ef6beca8f89366858e1a8e70cbd0b8812170c3
Aiden <aiden.chettleburgh720@gmail.com>
2025-11-27 15:50:47 -0500

    Added student test cases for LoginInteractor

--- src/main/java/data_access/FakeUserDataAccessObject.java
+++ src/main/java/data_access/FakeUserDataAccessObject.java
@@ -12,14 +12,24 @@ import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 public class FakeUserDataAccessObject implements SubmitUserDataAccessInterface, ResubmitUserDataAccessInterface,
                                                  LoginDataAccessInterface {
     Assignment assignment;
     boolean submitFailure;
+    // Login
+    private final Map<String, User> users;
+    List<Assignment> assignments;
+    Map<Assignment, Map<String, Submission>> submissionMap;
+    User activeUser;
 
-
+    // For Submit/Resubmit Testing
     public FakeUserDataAccessObject(boolean deadlinePassed, boolean submitFails) {
+        users = Map.of();
+        assignments = List.of();
+        submissionMap = Map.of();
+
         this.submitFailure = submitFails;
 
         LocalDateTime deadline;
@@ -38,6 +48,57 @@ public class FakeUserDataAccessObject implements SubmitUserDataAccessInterface, 
                 .build();
     }
 
+    // For Login Testing
+    public FakeUserDataAccessObject(LocalDateTime baseTime) {
+        this.users = Map.of(
+                "FakeStudent1", new User("FakeStudent1", "password", "fake", "one", User.STUDENT),
+                "FakeStudent2", new User("FakeStudent2", "password", "fake", "two", User.STUDENT),
+                "FakeInstructor", new User("FakeInstructor", "password", "fake", "instructor", User.INSTRUCTOR)
+        );
+        this.assignments = List.of(
+                Assignment.builder().name("FakeAssignment1")
+                        .description("FakeAssignment1 description")
+                        .creationDate(baseTime)
+                        .dueDate(baseTime.plusMinutes(30))
+                        .gracePeriod(10)
+                        .supportedFileTypes(List.of(".txt", ".java"))
+                        .build(),
+                Assignment.builder().name("FakeAssignment2")
+                        .description("FakeAssignment2 description")
+                        .creationDate(baseTime.minusMinutes(240))
+                        .dueDate(baseTime)
+                        .gracePeriod(0)
+                        .supportedFileTypes(List.of(".py"))
+                        .build()
+        );
+        this.submissionMap = Map.of(
+                assignments.get(0), Map.of(
+                        "FakeStudent1", Submission.getBuilder().submitter("FakeStudent1")
+                                .submissionTime(baseTime.minusMinutes(50))
+                                .submissionName("pledge.txt")
+                                .submissionData("This work is 100% my own (except the part my AI best friend did).")
+                                .status(Submission.ON_TIME)
+                                .build()
+                ),
+                assignments.get(1), Map.of(
+                        "FakeStudent1", Submission.getBuilder().submitter("FakeStudent1")
+                                .submissionTime(baseTime.minusMinutes(50))
+                                .submissionName("sample.py")
+                                .submissionData("print(\"Hello, World!\")")
+                                .status(Submission.GRADED)
+                                .grade(21.0)
+                                .feedback("very relevant feedback")
+                                .build(),
+                        "FakeStudent2", Submission.getBuilder().submitter("FakeStudent2")
+                                .submissionTime(baseTime)
+                                .submissionName("coolio.py")
+                                .submissionData("phrase = \"Hello Python!\"\nprint(phrase)\n")
+                                .status(Submission.LATE)
+                                .build()
+                )
+        );
+    }
+
     @Override
     public void submit(File studentFile) throws IOException {
         if (submitFailure) {
@@ -52,26 +113,33 @@ public class FakeUserDataAccessObject implements SubmitUserDataAccessInterface, 
 
     @Override
     public boolean existsByName(String username) {
-        return false;
+        return this.users.containsKey(username);
     }
 
     @Override
     public User getUser(String username) {
-        return null;
+        return this.users.get(username);
     }
 
     @Override
     public void setActiveUser(User user) {
-        // Does nothing :D
+        this.activeUser = user;
     }
 
     @Override
     public List<Assignment> getAssignments() {
-        return List.of();
+        return this.assignments;
     }
 
     @Override
     public Submission getSubmission(Assignment assignment) {
+        if (this.submissionMap.containsKey(assignment)) {
+            Map<String, Submission> assignmentSubmissions = this.submissionMap.get(assignment);
+            String username = activeUser.getName();
+            if (assignmentSubmissions.containsKey(username)) {
+                return assignmentSubmissions.get(username);
+            }
+        }
         return null;
     }
 }
---
+++ src/test/java/usecase/login/LoginInteractorTest.java
@@ -0,0 +1,203 @@
+package usecase.login;
+
+import data_access.FakeUserDataAccessObject;
+import entity.User;
+import interface_adapter.login.LoginPresenter;
+import org.junit.jupiter.api.Test;
+import usecase.login.*;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class LoginInteractorTest {
+
+    @Test
+    void testStudent1() {
+        String username = "FakeStudent1";
+        String password = "password";
+        LoginInputData inputData = new LoginInputData(username, password);
+        LocalDateTime time = LocalDateTime.now();
+        LoginDataAccessInterface fakeDAO = new FakeUserDataAccessObject(time);
+
+        LoginOutputBoundary presenter = new LoginOutputBoundary() {
+            @Override
+            public void switchToLoggedInView(LoginOutputData response) {
+                // assert that values are expected
+                assertEquals(username, response.getUsername(), "Usernames don't match.");
+                assertEquals("student", response.getUserType(), "User type is incorrect.");
+                Object[][] expectedAssignments = {
+                        {"FakeAssignment1", time.plusMinutes(30), "-", "✔"},
+                        {"FakeAssignment2", time, "21.0", "✔"}
+                };
+
+                Object[][] actualAssignments = response.getAssignments();
+                for (int i = 0; i < expectedAssignments.length; i++) {
+                    assertEquals(actualAssignments[i][0], expectedAssignments[i][0], "Assignment names don't match.");
+                    assertEquals(actualAssignments[i][1], expectedAssignments[i][1], "Due dates don't match.");
+                    assertEquals(actualAssignments[i][2], expectedAssignments[i][2], "Grades don't match.");
+                    assertEquals(actualAssignments[i][3], expectedAssignments[i][3], "Submission Statuses don't match.");
+                }
+            }
+
+            @Override
+            public void prepareFailView(String errorMessage) {
+                fail("Interactor gave error: " + errorMessage + "\nBut should have been successful.");
+            }
+        };
+
+        LoginInputBoundary interactor = new LoginInteractor(fakeDAO, presenter);
+        interactor.execute(inputData);
+    }
+
+    @Test
+    void testStudent2() {
+        String username = "FakeStudent2";
+        String password = "password";
+        LoginInputData inputData = new LoginInputData(username, password);
+        LocalDateTime time = LocalDateTime.now();
+        LoginDataAccessInterface fakeDAO = new FakeUserDataAccessObject(time);
+
+        LoginOutputBoundary presenter = new LoginOutputBoundary() {
+            @Override
+            public void switchToLoggedInView(LoginOutputData response) {
+                // assert that values are expected
+                assertEquals(username, response.getUsername(), "Usernames don't match.");
+                assertEquals("student", response.getUserType(), "User type is incorrect.");
+                Object[][] expectedAssignments = {
+                        {"FakeAssignment1", time.plusMinutes(30), "", "✖"},
+                        {"FakeAssignment2", time, "-", "✔"}
+                };
+
+                Object[][] actualAssignments = response.getAssignments();
+                for (int i = 0; i < expectedAssignments.length; i++) {
+                    assertEquals(actualAssignments[i][0], expectedAssignments[i][0], "Assignment names don't match.");
+                    assertEquals(actualAssignments[i][1], expectedAssignments[i][1], "Due dates don't match.");
+                    assertEquals(actualAssignments[i][2], expectedAssignments[i][2], "Grades don't match.");
+                    assertEquals(actualAssignments[i][3], expectedAssignments[i][3], "Submission Statuses don't match.");
+                }
+            }
+
+            @Override
+            public void prepareFailView(String errorMessage) {
+                fail("Interactor gave error: " + errorMessage + "\nBut should have been successful.");
+            }
+        };
+
+        LoginInputBoundary interactor = new LoginInteractor(fakeDAO, presenter);
+        interactor.execute(inputData);
+    }
+
+    @Test
+    void testInstructor() {
+        String username = "FakeStudent2";
+        String password = "password";
+        LoginInputData inputData = new LoginInputData(username, password);
+        LocalDateTime time = LocalDateTime.now();
+        LoginDataAccessInterface fakeDAO = new FakeUserDataAccessObject(time);
+
+        LoginOutputBoundary presenter = new LoginOutputBoundary() {
+            @Override
+            public void switchToLoggedInView(LoginOutputData response) {
+                // assert that values are expected
+                assertEquals(username, response.getUsername(), "Usernames don't match.");
+                assertEquals("student", response.getUserType(), "User type is incorrect.");
+                Object[][] expectedAssignments = {
+                        {"FakeAssignment1", time.plusMinutes(30), "", "✖"},
+                        {"FakeAssignment2", time, "-", "✔"}
+                };
+
+                Object[][] actualAssignments = response.getAssignments();
+                for (int i = 0; i < expectedAssignments.length; i++) {
+                    assertEquals(actualAssignments[i][0], expectedAssignments[i][0], "Assignment names don't match.");
+                    assertEquals(actualAssignments[i][1], expectedAssignments[i][1], "Due dates don't match.");
+                    assertEquals(actualAssignments[i][2], expectedAssignments[i][2], "Grades don't match.");
+                    assertEquals(actualAssignments[i][3], expectedAssignments[i][3], "Submission Statuses don't match.");
+                }
+            }
+
+            @Override
+            public void prepareFailView(String errorMessage) {
+                fail("Interactor gave error: " + errorMessage + "\nBut should have been successful.");
+            }
+        };
+
+        LoginInputBoundary interactor = new LoginInteractor(fakeDAO, presenter);
+        interactor.execute(inputData);
+    }
+
+//    @Test
+//    void deadlineFailCase() {
+//        @SuppressWarnings("ConstantConditions")
+//        File file = new File(getClass().getResource("/submitCaseTestFile1.txt").getPath());
+//
+//        SubmitInputData inputData = new SubmitInputData(LocalDateTime.now(), file);
+//        SubmitUserDataAccessInterface fakeDAO = new FakeUserDataAccessObject(true, false);
+//
+//        SubmitOutputBoundary presenter = new SubmitOutputBoundary() {
+//            @Override
+//            public void prepareSuccessView(SubmitOutputData pack) {
+//                fail(FailMsg);
+//            }
+//
+//            @Override
+//            public void prepareFailureView(SubmitOutputData pack) {
+//                assertEquals(SubmitInteractor.DDL_PASSED_MSG, pack.getOutputMsg());
+//            }
+//
+//        };
+//
+//        SubmitInputBoundary interactor = new SubmitInteractor(fakeDAO, presenter);
+//        interactor.execute(inputData);
+//    }
+//
+//    @Test
+//    void networkFailCase() {
+//        @SuppressWarnings("ConstantConditions")
+//        File file = new File(getClass().getResource("/submitCaseTestFile1.txt").getPath());
+//
+//        SubmitInputData inputData = new SubmitInputData(LocalDateTime.now(), file);
+//        SubmitUserDataAccessInterface fakeDAO = new FakeUserDataAccessObject(false, true);
+//
+//        SubmitOutputBoundary presenter = new SubmitOutputBoundary() {
+//            @Override
+//            public void prepareSuccessView(SubmitOutputData pack) {
+//                fail(FailMsg);
+//            }
+//
+//            @Override
+//            public void prepareFailureView(SubmitOutputData pack) {
+//                assertEquals(SubmitInteractor.NETWORK_ERROR_MSG, pack.getOutputMsg());
+//            }
+//
+//        };
+//
+//        SubmitInputBoundary interactor = new SubmitInteractor(fakeDAO, presenter);
+//        interactor.execute(inputData);
+//    }
+//
+//    @Test
+//    void FileTypeFailCase() {
+//        @SuppressWarnings("ConstantConditions")
+//        File file = new File(getClass().getResource("/submitCaseTestFile2.abcdefg").getPath());
+//        SubmitInputData inputData = new SubmitInputData(LocalDateTime.now(), file);
+//        SubmitUserDataAccessInterface fakeDAO = new FakeUserDataAccessObject(false, true);
+//
+//        SubmitOutputBoundary presenter = new SubmitOutputBoundary() {
+//            @Override
+//            public void prepareSuccessView(SubmitOutputData pack) {
+//                fail(FailMsg);
+//            }
+//
+//            @Override
+//            public void prepareFailureView(SubmitOutputData pack) {
+//                assertEquals(SubmitInteractor.WRONG_FILE_MSG, pack.getOutputMsg());
+//            }
+//
+//        };
+//
+//        SubmitInputBoundary interactor = new SubmitInteractor(fakeDAO, presenter);
+//        interactor.execute(inputData);
+//    }
+}
