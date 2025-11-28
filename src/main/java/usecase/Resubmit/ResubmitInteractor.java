bcd023b927ddf91b201ed794423a1957c50d77ea
Xihang Zhou <zhouxihang0728@outlook.com>
2025-11-27 23:49:04 -0500

    Use cases refracted (except interface and classes others are working on)

--- src/main/java/usecase/Resubmit/ResubmitInteractor.java
+++ src/main/java/usecase/Resubmit/ResubmitInteractor.java
@@ -13,10 +13,17 @@ public class ResubmitInteractor implements ResubmitInputBoundary {
         this.resubmitOutputBoundary = resubmitOutputBoundary;
         this.resubmitUserDataAccess = resubmitUserDataAccess;
     }
+
+    /**
+     * Executes the resubmit use case by checking if the provided resubmission time
+     * is before the assignment deadline. If the resubmission is within the allowed
+     * time frame, the view is switched to the submission success view. Otherwise,
+     * a failure message is prepared.
+     *
+     * @param inputData the input data containing the timestamp of the resubmission
+     */
     public void execute(ResubmitInputData inputData) {
-        LocalDateTime deadline = resubmitUserDataAccess.getAssignment().getDueDate();
-        // TODO: if we need to add disability part, we can adjust color of message
-        //  but if we do not need that, then we can delete ResubmitOutputData
+        final LocalDateTime deadline = resubmitUserDataAccess.getAssignment().getDueDate();
         if (deadline.isAfter(inputData.getTime())) {
             switchToSubmitView();
         }
@@ -24,6 +31,12 @@ public class ResubmitInteractor implements ResubmitInputBoundary {
             resubmitOutputBoundary.prepareFailView(ERROR_MESSAGE);
         }
     }
+
+    /**
+     * Switches the user interface to the submission success view.
+     * This method is invoked when the resubmission is confirmed to be within the allowed time frame.
+     * It delegates the preparation of the success view to the output boundary.
+     */
     public void switchToSubmitView() {
         resubmitOutputBoundary.prepareSuccessView();
     }
