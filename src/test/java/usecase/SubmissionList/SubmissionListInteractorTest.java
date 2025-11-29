545bf1427f7c0c5f2806239df05ab9b7664e9c08
Indy <48374401+Indy801@users.noreply.github.com>
2025-11-29 16:55:23 -0500

    Added unit test for submission list interactor.

---
+++ src/test/java/usecase/SubmissionList/SubmissionListInteractorTest.java
@@ -0,0 +1,78 @@
+package usecase.SubmissionList;
+
+import entity.Submission;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class SubmissionListInteractorTest {
+    private final SubmissionListDataAccessInterface testDAO =
+            new SubmissionListDataAccessInterface() {
+                @Override
+                public Submission getSubmissionForSubmissionView(String submitter) {
+                    if (submitter.equals("indy")) {
+                        return new Submission(
+                                "indy",
+                                LocalDateTime.of(2025,11,29, 16, 47, 0),
+                                "submission.java",
+                                "123456\naabbcc",
+                                86.37,
+                                Submission.GRADED,
+                                "Good job!"
+                        );
+                    }
+                    else {
+                        fail("Did not pass correct submitter to the DAO.");
+                        return null;
+                    }
+                }
+            };
+
+    @Test
+    void testBack() {
+        SubmissionListOutputBoundary boundary = new SubmissionListOutputBoundary() {
+            public void prepareSubmissionView (SubmissionListOutputData data){
+                fail("Did not call the correct presenter method.");
+            }
+            public void goToAssignmentView () {}
+        };
+
+        SubmissionListInputData testInputData = new SubmissionListInputData(true, null, null);
+
+        SubmissionListInteractor interactor = new SubmissionListInteractor(boundary, testDAO);
+        interactor.execute(testInputData);
+    }
+
+    @Test
+    void testSelectSubmission() {
+        final SubmissionListOutputBoundary boundary = new SubmissionListOutputBoundary() {
+            public void prepareSubmissionView (SubmissionListOutputData data){
+                assertEquals("Test Assignment", data.getAssignmentName());
+                assertEquals("indy", data.getSubmitter());
+                assertEquals(Submission.GRADED.toString(), data.getStatus());
+                assertEquals(
+                        LocalDateTime.of(2025,11,29, 16, 47, 0)
+                                .format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss")),
+                        data.getSubmissionDate()
+                );
+                assertEquals("submission.java", data.getSubmissionName());
+                assertEquals("86.4", data.getGrade());
+                assertEquals("Good job!", data.getFeedback());
+                assertEquals("100", data.getMaxGrade()); // Hardcoded
+            }
+            public void goToAssignmentView () {
+                fail("Did not call the correct presenter method.");
+            }
+        };
+
+        SubmissionListInputData testInputData = new SubmissionListInputData(false,
+                "indy", "Test Assignment");
+
+        SubmissionListInteractor interactor = new SubmissionListInteractor(boundary, testDAO);
+        interactor.execute(testInputData);
+    }
+}
